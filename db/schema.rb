2a7cdafbd5e5804b5d707088ed54502d272f7709
David Ladowitz <david@ladowitz.com>
2015-03-12 17:42:56 -0700

    cleaning up

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,6 @@ ActiveRecord::Schema.define(version: 20150311235629) do
     t.datetime "updated_at"
   end
 
-  create_table "registrations", force: true do |t|
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
--- spec/models/registration_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Registration do
-  it { should validate_presence_of :email }
-
-  it "has a valid factory" do
-    expect(create :registration).to be_an_instance_of Registration
-  end
-end
